a7220fbe210b68cd998f929698c399cbfff57fbe
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-04-17 15:34:33 +0200

    feat: Add main site components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import About from '@/components/site-components/About';
+import FAQs from '@/components/site-components/FAQs';
+import Footer from '@/components/site-components/Footer';
+import Hero from '@/components/site-components/Hero';
+import Navbar from '@/components/site-components/Navbar';
+import OurTeam from '@/components/site-components/OurTeam';
+import ProjectVideo from '@/components/site-components/ProjectVideo';
+import Statistics from '@/components/site-components/Statistics';
 import React from 'react';
-import Navbar from './components/site-components/Navbar';
-import Hero from './components/site-components/Hero';
-import About from './components/site-components/About';
-import OurTeam from './components/site-components/OurTeam';
-import FAQs from './components/site-components/FAQs';
-import Statistics from './components/site-components/Statistics';
-import ProjectVideo from './components/site-components/ProjectVideo';
-import Footer from './components/site-components/Footer';
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center ">
